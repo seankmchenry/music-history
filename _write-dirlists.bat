069592177103c17ece5b8b522c2c4552ffcad44b
Sean McHenry <seankmchenry@gmail.com>
2015-07-17 01:49:25 -0400

    Added old albums on external hard drive

--- _write-dirlists.bat
+++ _write-dirlists.bat
@@ -1,8 +1,10 @@
-dir /B /S /TC *.mp3 D:\Dropbox\Music\Albums > .\albums.txt
-dir /B /S /TC *.mp3 D:\Dropbox\Music\Compilations >> .\albums.txt
+dir /B /S *.mp3 D:\Dropbox\Music\Albums > .\albums.txt
+dir /B /S *.mp3 D:\Dropbox\Music\Compilations >> .\albums.txt
 
-dir /B /S /TC *.mp3 D:\Dropbox\Music\Singles > .\singles.txt
+dir /B /S *.mp3 D:\Dropbox\Archived\Music\Albums > .\albums_archived.txt
 
-dir /B /S /TC *.mp3 D:\Dropbox\Archived\Music\Albums > .\albums_archived.txt
+dir /B /S *.mp3 D:\Dropbox\Music\Singles > .\singles.txt
 
-dir /B /S /TC *.mp3 D:\Dropbox\Archived\Music\Singles > .\singles_archived.txt
+dir /B /S *.mp3 D:\Dropbox\Archived\Music\Singles > .\singles_archived.txt
+
+dir /B /S *.mp3 S:\Music\Albums > .\albums_old.txt
