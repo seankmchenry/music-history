868417956a65d5302d809f92b484045c837c4269
Sean McHenry <seankmchenry@gmail.com>
2015-07-17 00:22:29 -0400

    Initial commit

---
+++ _write-dirlists.bat
@@ -0,0 +1,8 @@
+dir /B /S /TC *.mp3 D:\Dropbox\Music\Albums > .\albums.txt
+dir /B /S /TC *.mp3 D:\Dropbox\Music\Compilations >> .\albums.txt
+
+dir /B /S /TC *.mp3 D:\Dropbox\Music\Singles > .\singles.txt
+
+dir /B /S /TC *.mp3 D:\Dropbox\Archived\Music\Albums > .\albums_archived.txt
+
+dir /B /S /TC *.mp3 D:\Dropbox\Archived\Music\Singles > .\singles_archived.txt
