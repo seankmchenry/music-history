535a7b9ee974f1861c2474191fce37d3c68544ac
Sean McHenry <seankmchenry@gmail.com>
2015-07-17 14:37:15 -0400

    Updated backup task, reorganized and added covers

--- _run-backup.bat
+++ _run-backup.bat
@@ -1,5 +1,6 @@
 dir /B /S *.mp3 D:\Dropbox\Music\Albums > .\albums.txt
 dir /B /S *.mp3 D:\Dropbox\Music\Compilations >> .\albums.txt
+dir /B /S *.mp3 D:\Dropbox\Music\EPs >> .\albums.txt
 
 dir /B /S *.mp3 D:\Dropbox\Archived\Music\Albums > .\albums_archived.txt
 
@@ -7,4 +8,6 @@ dir /B /S *.mp3 D:\Dropbox\Music\Singles > .\singles.txt
 
 dir /B /S *.mp3 D:\Dropbox\Archived\Music\Singles > .\singles_archived.txt
 
-dir /B /S *.mp3 S:\Music\Albums > .\albums_old.txt
+dir /B /S *.mp3 S:\Music > .\music_old.txt
+
+xcopy /S /Y S:\Music\Compilations\*.jpg .\covers\
