3acf0aa3c444fe6a147032498b274ec73abfffc5
Sean McHenry <seankmchenry@gmail.com>
2015-07-17 18:09:36 -0400

    Archived Max Mohr and others

--- _run-backup.bat
+++ _run-backup.bat
@@ -1,13 +1,20 @@
+rem /* Albums */
 dir /B /S *.mp3 D:\Dropbox\Music\Albums > .\albums.txt
 dir /B /S *.mp3 D:\Dropbox\Music\Compilations >> .\albums.txt
 dir /B /S *.mp3 D:\Dropbox\Music\EPs >> .\albums.txt
 
-dir /B /S *.mp3 D:\Dropbox\Archived\Music\Albums > .\albums_archived.txt
-
+rem /* Singles */
 dir /B /S *.mp3 D:\Dropbox\Music\Singles > .\singles.txt
 
+rem /* Albums (Archived) */
+dir /B /S *.mp3 D:\Dropbox\Archived\Music\Albums > .\albums_archived.txt
+dir /B /S *.mp3 D:\Dropbox\Archived\Music\EPs >> .\albums_archived.txt
+
+rem /* Singles (Archived) */
 dir /B /S *.mp3 D:\Dropbox\Archived\Music\Singles > .\singles_archived.txt
 
+rem /* Old Music */
 dir /B /S *.mp3 S:\Music > .\music_old.txt
 
+rem /* Covers */
 xcopy /S /Y S:\Music\Compilations\*.jpg .\covers\
