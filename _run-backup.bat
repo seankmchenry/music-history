f972f13ecee7007b558de6d83208410dd38b37ce
Sean McHenry <seankmchenry@gmail.com>
2015-07-19 04:19:13 -0400

    Updated backup task and deleted 'BPM' directory

--- _run-backup.bat
+++ _run-backup.bat
@@ -1,17 +1,20 @@
+@echo off
+set DIRCMD=/B /S
+
 rem /* Albums */
-dir /B /S *.mp3 D:\Dropbox\Music\Albums > .\albums.txt
-dir /B /S *.mp3 D:\Dropbox\Music\Compilations >> .\albums.txt
-dir /B /S *.mp3 D:\Dropbox\Music\EPs >> .\albums.txt
+dir *.mp3 D:\Dropbox\Music\Albums > albums.txt 2>nul
+dir *.mp3 D:\Dropbox\Music\Compilations >> albums.txt 2>nul
+dir *.mp3 D:\Dropbox\Music\EPs >> albums.txt 2>nul
 
 rem /* Singles */
-dir /B /S *.mp3 D:\Dropbox\Music\Singles > .\singles.txt
+dir *.mp3 D:\Dropbox\Music\Singles > singles.txt 2>nul
 
 rem /* Albums (Archived) */
-dir /B /S *.mp3 D:\Dropbox\Archived\Music\Albums > .\albums_archived.txt
-dir /B /S *.mp3 D:\Dropbox\Archived\Music\EPs >> .\albums_archived.txt
+dir *.mp3 D:\Dropbox\Archived\Music\Albums > albums_archived.txt 2>nul
+dir *.mp3 D:\Dropbox\Archived\Music\EPs >> albums_archived.txt 2>nul
 
 rem /* Singles (Archived) */
-dir /B /S *.mp3 D:\Dropbox\Archived\Music\Singles > .\singles_archived.txt
+dir *.mp3 D:\Dropbox\Archived\Music\Singles > singles_archived.txt 2>nul
 
 rem /* Old Music */
-IF EXIST S:\ dir /B /S *.mp3 S:\Music > .\music_old.txt
+IF EXIST S:\ dir *.mp3 S:\Music > music_old.txt 2>nul
