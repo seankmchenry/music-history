8bcf5553533663d19b66e1a0c520357aeb8b69e7
Sean McHenry <seankmchenry@gmail.com>
2016-12-30 18:23:29 -0500

    Remapped music folder paths after switching PC.

--- _run-backup.bat
+++ _run-backup.bat
@@ -2,16 +2,16 @@
 set DIRCMD=/B /S
 
 rem /* Albums */
-dir *.mp3 D:\Music\Albums > albums.txt 2>nul
+dir *.mp3 C:\Users\Sean\Music\Albums > albums.txt 2>nul
 
 rem /* Singles */
-dir *.mp3 D:\Music\Singles > singles.txt 2>nul
+dir *.mp3 C:\Users\Sean\Music\Singles > singles.txt 2>nul
 
 rem /* Albums (Archived) */
-dir *.mp3 D:\Music\[Archived]\Albums > albums_archived.txt 2>nul
+dir *.mp3 C:\Users\Sean\Music\[Archived]\Albums > albums_archived.txt 2>nul
 
 rem /* Singles (Archived) */
-dir *.mp3 D:\Music\[Archived]\Singles > singles_archived.txt 2>nul
+dir *.mp3 C:\Users\Sean\Music\[Archived]\Singles > singles_archived.txt 2>nul
 
 rem /* Old Music */
 IF EXIST S:\ dir *.mp3 S:\Music > music_old.txt 2>nul
